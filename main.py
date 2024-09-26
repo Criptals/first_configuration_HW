0a5073ed2f019ade7a85f540308b1b5fdb579485
Кудрявцев Сергей <sergei.kudryavtsev@gmail.com>
2024-09-26 16:58:13 +0300

    update GUI; add main file, zip archive and path file

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-import tkinter as tk
-from tkinter.scrolledtext import ScrolledText
-import subprocess
+from zipfile import ZipFile
+from sys import argv
+from os.path import exists
+from win_mode import Window
+from terminal import MyTerminal
 
-class TerminalApp(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.geometry("600x400")
-        self.title("Embedded Terminal")
 
-        self.terminal_output = ScrolledText(self, wrap=tk.WORD)
-        self.terminal_output.pack(fill=tk.BOTH, expand=True)
+def main():
+    if len(argv) > 1:
+        config_file = argv[1]
+    else:
+        print("Отсутствует необходимый аргумент: путь к конфигурационному файлу")
+        return
 
-        self.terminal_process = subprocess.Popen(["bash"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    if exists(config_file):
+        with open(config_file) as config:
+            fs_path = config.readline().strip()
+    else:
+        print("Конфигурационный файл с таким названием отсутствует")
+        return
 
-        self.after(100, self.update_terminal())
-
-    def update_terminal(self):
-        data = self.terminal_process.stdout.read(1024)
-        if data:
-            self.terminal_output.insert(tk.END, data.decode())
-        self.after(100, self.update_terminal())
-
-if __name__ == "__main__":
-    app = TerminalApp()
-    app.mainloop()
+if __name__ == '__main__':
+    main()
